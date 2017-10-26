17692a9b6c18977858d0e22a6a61ca6077d28df2
Jayaraman <vinothjayaram@gmail.com>
2017-10-26 18:00:32 -0400

    created the manage component & form

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import Layout from './components/layout';
 import HomePage from './components/Home/HomePage';
 import AboutPage from './components/About/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCourse from './components/course/ManageCoursePage';
 
 export default (
     <Route path="/" component={Layout}>
       <IndexRoute component={HomePage}/>
       <Route path="about" component={AboutPage}/>
       <Route path="courses" component={CoursesPage}/>
+      <Route path="course/:id" component={ManageCourse}/>
     </Route>
   );
